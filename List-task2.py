00b75bdff46d105c0af67662f7fc49b26ab881e8
Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-09-21 00:49:33 +0600

    Add files via upload

---
+++ List-task2.py
@@ -0,0 +1,9 @@
+first_list = [123, 'Other world', True]
+second_list = [None, 77.22, False]
+# first variant output
+merged_list = first_list + second_list
+# second variant output
+print(first_list.__add__(second_list))
+
+print(merged_list)
+

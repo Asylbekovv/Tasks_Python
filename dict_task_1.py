9fd01adc22c5372a53e7707a7fa463b339dcbdb2
Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-09-22 20:12:29 +0600

    Add files via upload

---
+++ dict_task_1.py
@@ -0,0 +1,14 @@
+my_dict = {}
+request_key_1 = input("Введите ключ: ")
+request_meaning_1 = input("Введите значение: ")
+my_dict[request_key_1] = request_meaning_1
+
+request_key_2 = input("Введите ключ: ")
+request_meaning_2 = input("Введите значение: ")
+my_dict[request_key_2] = request_meaning_2
+
+request_key_3 = input("Введите ключ: ")
+request_meaning_3 = input("Введите значение: ")
+my_dict[request_key_3] = request_meaning_3
+print(my_dict)
+

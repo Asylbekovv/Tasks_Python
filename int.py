dcc3d27dacabb3f057fc81fb049c46bfb5113d16
Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-09-26 00:40:01 +0600

    Add files via upload

--- int.py
+++ int.py
@@ -8,14 +8,30 @@ print(id(number_two))
 my_list = [1, 2, 3]
 print(id(my_list))
 print(type(my_list))
-print()
-other_list = [1, 2, 3]
-other_list.append(4)
+other_list = [1, 2, 3, 4]
 print(id(other_list))
 print(type(my_list))
 new_list = [1, 2, 3]
 print(id(new_list))
 print(type(my_list))
+
+# Dicts
+info = {'user': 'Arstan', 'is_instructor': True}
+info_copy = info
+info['reviews_qty'] = 10
+info_copy['reviews_qty'] = 400
+print(info)
+print(info_copy)
+# Working with Dicts and conversion in List
+info_second = {'user': 'Jack', 'is_instructor': False}
+info_second_new = info_second
+info_second_new['Work'] = True
+conversion_list = list(info_second_new)
+print(conversion_list)
+print(type(conversion_list))
+print(id(conversion_list) > id(info_second_new))
+
+
 
 
 

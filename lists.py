b34e77d7a162602554d0dd219059d5d2779fcb31
Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-09-26 12:50:24 +0600

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,21 @@
+# Нахождение среднего значения списка
+print("Наш список")
+this_list = [1, 22, 56, 99, 54]
+print(this_list)
+result = sum(this_list)
+convert_int = int(result)
+list_len = len(this_list)
+average_total = result / list_len
+print('Среднее арифметическое списка: ')
+print(average_total)
+
+#Нахождение наибольшего и наименшего элемента в
+print("Нахождение наибольшего и наименшего элемента в списке")
+greatest_list = [32, 13, 45, 654, 565, 534]
+print("Наибольший элемент")
+print(max(greatest_list))
+print("Наименьший элемент")
+print(min(greatest_list))
+print("Длинна списка")
+print(len(greatest_list))
+

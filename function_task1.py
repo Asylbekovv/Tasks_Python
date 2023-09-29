2e0d1570021a57dee36195f168c0233581219528
Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-09-30 02:53:49 +0600

    Add files via upload

--- function_task1.py
+++ function_task1.py
@@ -17,6 +17,7 @@ def intersect(set_1, set_2):
     convert_list = list(intersect_set)
     return set_1, set_2
 
+
 new_set_first = {111, True, 423}
 new_set_second = {4324, None}
 print(intersect(new_set_first, new_set_second))
@@ -25,6 +26,7 @@ print(intersect(new_set_first, new_set_second))
 def know_tuple_count(tuple_count):
     print(tuple_count.count(f"{first_tuple}"))
     return tuple_count
+
 
 first_tuple = ('Asylbekov', 123, 24.23)
 print(know_tuple_count(first_tuple))
@@ -38,8 +40,30 @@ def list_to_dict(user_1, user_2):
     conv_dict = conv_zip
     return user_1, user_2
 
+
 user_1_l = [4354, True, False]
 user_2_l = [242, 'abc', None, True]
 result_dict = (user_1_l, user_2_l)
 print(list_to_dict(user_1_l, user_2_l))
 print(type(result_dict))
+
+
+def summ(*all_sum):
+    print(all_sum)
+    print(type(all_sum))
+    return sum(all_sum)
+
+
+print(summ(1234, 3444, 4664, 6757))
+
+
+def name_and_surname(name, surname):
+    print(f"Здраствуйте {surname}  {name}")
+    return surname, name
+
+
+name_user = input("Как тебя зовут: ")
+sur_name = input("Твоя фамилия: ")
+
+print(name_and_surname(name_user, sur_name))
+

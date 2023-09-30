04adfaf17d74c422331e79316556faa364541d0d
Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-10-01 02:29:15 +0600

    Add files via upload

--- sets-3.py
+++ sets-3.py
@@ -7,4 +7,30 @@ second_list = [3, 4, 5, 6, 7]
 all_elem = list(set(first_list) & set(second_list))
 print(all_elem)
 
+def sum_nums(*args):
+    print(args)
+    print(type(args))
+    return sum(args)
+
+
+print(sum_nums(122, 323, 667))
+
+
+def post_qty_info(name, posts):
+    info = f"Hello {name} you have {posts} posts"
+    return info
+
+info_user = post_qty_info('Arstan', 220)
+print(info_user)
+print(type(info_user))
+
+def get_email_pass(number, email, password):
+    get_info = f"User: {number} Email: {email} \nPassword: {password}\n"
+    return get_info
+
+user_01 = get_email_pass(number=1, email="asylbekov20love@gmail.com", password=2102023)
+user_02 = get_email_pass(number=2, email="kamila009@gmail.com", password=2331324)
+user_03 = get_email_pass(number=3, email="aidanaofficial223@gmail.com", password=32423453)
+
+print(user_01, user_02,user_03)
 

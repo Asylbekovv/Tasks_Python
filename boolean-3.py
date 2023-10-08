47b0f484d3f7ecb28fa8b1fa959b1040e90a2f80
Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-10-08 22:22:46 +0600

    Add files via upload

---
+++ boolean-3.py
@@ -0,0 +1,47 @@
+# The sun rises in the east(1)
+statement_sun = "The sun rises in the east"
+is_true_sun = True
+if statement_sun != is_true_sun:
+    print("True")
+else:
+    print("False")
+
+# Comparison of numbers(2)
+statement_num1 = 5
+statement_num2 = 3
+print(statement_num1 > statement_num2)
+statement_num = 5 > 3
+print(bool(statement_num))
+
+# Snow can be hot(3)
+snow_hot = "Snow can be hot"
+is_snow = False
+if snow_hot == "Snow can be hot":
+    is_snow = True
+
+if is_snow:
+    print("False")
+else:
+    print("True")
+
+# All married mens(4)
+merry = "All married men have a wedding ring on their finger"
+is_merry_true = True
+if merry == "All married men have a wedding ring on their finger":
+    print("True")
+
+if is_merry_true == "All married men have a wedding ring on their finger":
+    print("True")
+else:
+    print("False")
+
+# An apple is heavier than a peach(5)
+apple = "An apple is heavier than a peach"
+is_apple = True
+if apple == "An apple is heavier than a peach":
+    print("True")
+
+if is_apple == "An apple is heavier than a peach":
+    print("True")
+else:
+    print("False")

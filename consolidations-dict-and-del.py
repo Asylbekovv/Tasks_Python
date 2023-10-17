1ca31fedbdb7e902df5ba27d4e46025dad30adca
Asylbekov Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-10-17 23:49:51 +0600

    Add files via upload

---
+++ consolidations-dict-and-del.py
@@ -0,0 +1,19 @@
+# How long does a person live country in use dict and union
+country_dict = {
+    'USA': 'Washington',
+    'Spain': 'Madrid',
+    'Italy': 'Rome',
+    'Kyrgyzstan': 'Bishkek',
+    'France': 'Paris',
+    'England': 'London'
+}
+
+people_in_country = {
+    'Washington': "689545 peoples",
+    'Madrid': "328000 peoples",
+    'Rome': "2758000 peoples",
+    'Bishkek': "7000000 peoples",
+    'Paris': "2148327 peoples",
+    'London': "648110 peoples"
+}
+

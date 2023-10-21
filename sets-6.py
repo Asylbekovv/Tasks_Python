a10127d79abde3ab95482649ab691a573e0bfa73
Asylbekov Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-10-21 22:32:10 +0600

    Add files via upload

---
+++ sets-6.py
@@ -0,0 +1,28 @@
+# Найти уникальные элементы в списке
+unique_list = [100, 200, 300, 400, 400, 200, 300, 100]
+unique_set = set(unique_list)
+print(unique_set)
+
+
+# Найти уникальные слова в тексте
+
+
+def unique_words(text):  # Function for search unique words in text
+    words = text.lower().split()
+    unique_word = set(words)
+    return len(unique_word)
+
+
+unique_input = input("Enter your text: ")
+unique_output = unique_words(unique_input)
+
+print(f"Count unique words: {unique_output}")
+
+# Найти пересечение двух списков
+j_list = ['L', 'Manhwa', 'Manga', 'Paper', 'Pen']
+i_list = ['Manhwa', 'Manga', 'L', 'Pencil', 'Marker']
+j_i_intersection = set(j_list).intersection(set(i_list))
+print(f"Intersection list: {j_i_intersection}")
+
+
+

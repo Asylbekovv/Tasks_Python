a2788007053a097ec61c99eeb4d271c6fbcee0b5
Asylbekov Arstan <144426976+Asylbekovv@users.noreply.github.com>
2023-11-01 00:01:05 +0600

    Add files via upload

---
+++ empty-2.py
@@ -0,0 +1,34 @@
+'''''# Task with use concatenate str and output in console
+first_str = "Hello "
+second_str = "Arstan"
+concatenable_text = first_str + second_str
+print(concatenable_text)
+
+user_name = input("Enter your name: ")
+greeting_user = f"Hello {user_name} welcome to site"
+print(greeting_user)
+PASSWORD = 23423323
+user_password = input("Enter your password: ")
+output_password = f"Your account password: {user_password} is confirm"
+print(output_password)
+
+# Task with use sets methods
+a_set = {220, 343, 435, 456, 76, 757, 6456}
+b_set = {223, 343, 220, 354, 67, 6456, 566}
+intersection_sets = a_set.intersection(b_set)
+another_intersection = a_set | b_set
+print(another_intersection)
+print(intersection_sets)
+is_sub_set = b_set.issubset(a_set)
+print(is_sub_set)
+'''
+
+# Task with use tuples
+students = (('Alina', 67, 18),
+            ('Aidana', 100, 18),
+            ('Aidar', 55, 19),
+            ('Sultan', 88, 18))
+
+student_names = students.
+
+
